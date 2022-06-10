c0b3fd84d4920a3fb25f287f109bbac1a55b68ce
anikurhade <anikurhade2002@gmail.com>
2022-06-10 22:01:15 +0530

    Created Terms And Policy Activity- Anirudha

---
+++ app/src/main/java/com/example/minibis/termandpolicy.java
@@ -0,0 +1,14 @@
+package com.example.minibis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class termandpolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termandpolicy);
+    }
+}
