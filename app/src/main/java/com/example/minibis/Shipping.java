d1b5b5b011ac95541ac7cc261e978d34c2fc8c20
anikurhade <anikurhade2002@gmail.com>
2022-06-15 19:52:47 +0530

    Added Shipping Activity- Anirudha

---
+++ app/src/main/java/com/example/minibis/Shipping.java
@@ -0,0 +1,21 @@
+package com.example.minibis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Shipping extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shipping);
+    }
+    public void gotomship(View view)
+    {
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
+    }
+}
